0fffd7930935d848beb770dfbe8cd13193ee33bb
Eunhee <82435813+joywhy@users.noreply.github.com>
2024-07-26 09:38:51 +0900

    init: 초기 세팅 및 폴더 구성

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import {  Routes } from "react-router-dom";
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <Routes>
+          {/* <Route path="/" element={<Main/>}/>
+          <Route path="/products" element={<Bookmark/>}/>
+          <Route path="/bookmark" element={<ProductList/>}/> */}
+        </Routes>
+        
+    </>
+  )
+}
+
+export default App
