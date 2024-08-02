072510bade8fd603bbf1707eb5baac0d7346c0cf
LIEUL <leh9111@naver.com>
2024-08-02 16:21:12 +0900

    chore:

--- src/components/common/Contents.jsx
+++ src/components/common/Contents.jsx
@@ -6,31 +6,69 @@ import styled from 'styled-components';
 const Contents = ({List,index}) => {
   const isManager =  window.localStorage.getItem("cate")==="최고관리자";
   // console.log(List);
+  const userType =  window.localStorage.getItem("cate");
+  const advertiser = userType==="최고관리자"?"광고주":"";
+
   return (
 <StyledDiv>
    {
     isManager? 
-    <CampaignForm/>
+    // <CampaignForm name = {List[index].name} advertiser={advertiser}/>
+    <CampaignForm />
     :
     <CreatorList list = {List[index].creatorList}/>
    }
 </StyledDiv>
   )
 }
-const CampaignForm = ({name=""})=>{
+const CampaignForm = ({name="",advertiser=""})=>{
   const [values, setValues] = useState({
     name: name,
-    password: "",
+    advertiser: advertiser,
     channelList:[],
     memo:""
   })
-  const [errors, setErrors] = useState({
-    email: "",
-    password: "",
-  })
-  return (<>CampaignForm
+  // const [errors, setErrors] = useState({
+  //   email: "",
+  //   password: "",
+  // })
+  const handleChange = e => {
+    setValues({
+      ...values,
+      [e.target.name]: e.target.value,
+    })
+    // const errors = validate(values);
+    // setErrors(errors);
+  }
+
+  const handleSubmit =  e => {
+    e.preventDefault();
+  }
+  return (<StyledForm onSubmit={handleSubmit}>
+    
+   <input
+          type="text"
+          name="name"
+          value={values.name}
+          onChange={handleChange}
+          placeholder="캠페인명"
+        />
+         <input
+          type="text"
+          name="advertiser"
+          value={values.advertiser}
+          onChange={handleChange}
+          placeholder="@광고주"
+        />
+        <h3>채널 리스트</h3>
+        <ul>
+          
+        </ul>
+         <button type="submit"   className="text16" >완료</button>
   
-  </>);
+  </StyledForm>
+  
+  );
 }
 
 const CreatorList = ({list})=> {
@@ -79,6 +117,38 @@ const Li = (props)=>{
 
 const StyledDiv = styled.div`
 flex-grow:7;
+`;
+const StyledForm = styled.form`
+width: 100%;
+text-align: center;
+display: flex;
+flex-direction: column;
+align-items: center;
+padding: 20px;
+box-sizing: border-box;
+border:1px solid red;
+
+& >input {
+ width: 100%;
+ height: 40px;
+ border-radius:5px;
+ border: none;
+ font-size: 20px;
+ margin-bottom: 14px;
+ padding: 0 10px;
+ box-sizing: border-box;
+ background-color: #F5F5F5;
+ font-size: 14px;
+color: black;
+  &:focus {outline: 2px solid var(--main-mint);}
+ }
+ & h3 {
+  width: 100%;
+  text-align: left;
+  font-size: 16px;
+  font-weight: bold;
+  margin-top: 17px;
+ }
 `;
 
 const StyledContainer = styled.div`
