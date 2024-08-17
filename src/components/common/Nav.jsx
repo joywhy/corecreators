d696c87de9279db4eb8f7edb13eeafb16fa4d039
Eunhee <82435813+joywhy@users.noreply.github.com>
2024-08-17 13:08:02 +0900

    style: report

--- src/components/common/Nav.jsx
+++ src/components/common/Nav.jsx
@@ -177,13 +177,6 @@ const Header = ({
   }, []);
   if (showInput) {
     return (
-      //       <div class="search-wrapper">
-      //          <div class="input-holder">
-      //             <input type="text" class="search-input" placeholder="Type to search" />
-      //              <button class="search-icon" onclick="searchToggle(this, event);"><span></span></button>
-      //          </div>
-      //          <span class="close" onclick="searchToggle(this, event);"></span>
-      //       </div>
       <StyledHeader2 ref={inputRef} className={showInput ? 'active' : ''}>
         <input
           placeholder="Search..."
