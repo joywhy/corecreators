e4c58d0ef51dd8dfbd1a00416d99097d9717e8dc
Eunhee <82435813+joywhy@users.noreply.github.com>
2024-08-23 16:19:37 +0900

    fix: home  page change

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,19 +2,19 @@ export const CARDS = [
   {
     title: '리스트업 솔루션',
     desc: '크리에이터 리스트 전달용',
-    img: '/src/assets/home/list_img.png',
+    img: '/src/assets/home/listup.png',
     url: 'http://localhost:5173/list', //추후 주소로
   },
   {
     title: '보고서 솔루션',
     desc: '캠페인 보고서 전달용',
-    img: '/src/assets/home/list_img.png',
+    img: '/src/assets/home/report.png',
     url: 'http://localhost:5173/report',
   },
   {
     title: '코크 크리에이터',
     desc: '어떤 크리에이터들이 활동하고 있는 지 검색해보세요!',
-    img: '/src/assets/home/coreCreator_img.jpg',
+    img: '/src/assets/home/sort.png',
     url: 'http://localhost:5173/sort',
   },
 ];
